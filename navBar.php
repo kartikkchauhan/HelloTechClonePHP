6dc1a7faadd43868d97e535c078e88152c8f48e3
Kartik chauhan <kartikchauhan100@gmail.com>
2019-07-16 10:33:31 +0530

    Final Structure

---
+++ navBar.php
@@ -0,0 +1,9 @@
+<div class="navbar-header">
+	<a class="navbar-brand" href="index.php">Home</a>
+</div>
+<ul class="nav navbar-nav">
+	<li><a href="#">Home</a></li>
+	<li><a href="#">Page 1</a></li>
+	<li><a href="#">Page 2</a></li>
+	<li><a href="#">Page 3</a></li>
+</ul>
