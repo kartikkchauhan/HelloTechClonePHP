5589e7ea5ccd3cc6ea726ec85bbd4e82e3c8b119
Shonali_Bisht <38500932+Shonali-Bisht@users.noreply.github.com>
2019-07-18 22:56:59 +0530

    Add files via upload

--- businessplan.php
+++ businessplan.php
@@ -1,4 +1,4 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
   <head>
     <!-- Required meta tags -->
@@ -17,152 +17,163 @@
 
     <!--link rel="stylesheet" href="styling.css">-->
     <title>Hello, world!</title>
+
+    <style type="text/css">
+   
+    </style>
   </head>
-  <body style="overflow-x: hidden;">
+  <body>
     <div class="container">
-    <div class="signinhead">
-        <h1><span class="glyphicon glyphicon-home"></span> HelloTech</h1>
-    </div>
-    <div class="container">
-        <img src="459176-dell-precision-5720-aio.jpg" alt="Snow">
-            </div>
-    <div class="overimg">
-      <h1 style="z-index:+10">Tech Support For Business</h1>
-      <p>
-        Why hire a full-time IT team when HelloTech <br>
-        can cover your tech support needs?  HelloTech provides <br>
-        affordable tech support to businesses of all sizes that are <br>
-        looking to stay up and running. Have multiple locations? <br>
-        We are in all 50 states. <br>
-      </p>
+    	<div class="signinhead">
+        	<h1><span class="glyphicon glyphicon-home"></span> HelloTech</h1>
+    	</div>
+    	<div class="container">
+        	<img src="459176-dell-precision-5720-aio.jpg" alt="Snow" style="opacity: 0.5;">
+    	</div>
+    	
+    	<div class="row">
+      		<div id="overim" class="col-md-8" style="top: -400px;left: 25px;">
+        		<h1 id="idh1" style="">Tech Support For Business</h1>
+        		<p>
+         		 Why hire a full-time IT team when HelloTech <br>
+          		can cover your tech support needs?  HelloTech provides <br>
+          		affordable tech support to businesses of all sizes that are <br>
+          		looking to stay up and running. Have multiple locations? <br>
+          		We are in all 50 states. <br>
+        		</p>
+      		</div>
+      		<div class="col-md-4" >
+	        	<div class="planform">
+	            	<form class="" action="index.html" method="post">
+	            		<br>
+		              	<h3><b>TELL US ABOUT YOUR BUSINESS</b></h3>
+		              	<input type="text" name="" placeholder="Company Name">
+		             	<input type="text" name="" placeholder="First Name">
+		             	<input type="text" name="" placeholder="Last Name">
+		              	<input type="text" name="" placeholder="ZIP Code">
+		              	<input type="number" name="" placeholder="Number of Employees to Service">
+		              	<input type="text" name="" placeholder="What is your Email Solution">
+		              	<input type="text" name="" placeholder="Industry">
+		              	<input type="text" name="" placeholder="What Do You Do For Tech Support">
+		              	<input type="text" name="" placeholder="Website">
+		              	<input type="text" name="" placeholder="Email">
+		              	<input type="text" name="" placeholder="Phone Number">
+		                Which Plan Interests You? <br>
+		             	Business Basic <input name="group3" type="radio" > <br>
+		            	Business Premium <input name="group3" type="radio"><br>
+		                <textarea rows="3" cols="50" name="comment" form="usrform" placeholder="Enter text here..."></textarea>
+		                <input type="submit" name="SUBMIT">
+	            	</form>
+	        	</div>
+      		</div>
+    	</div>
+    	<div id="bottomcont" class="container">
+      		<div class="row" >
+	        	<div class="col-md-2" style="height: 0px;">
 
-    </div>
-    <div class="planform" >
-      <form class="" action="index.html" method="post">
-        <h3>TELL US ABOUT YOUR BUSINESS</h3>
-        <input type="text" name="" placeholder="Company Name">
-        <input type="text" name="" placeholder="First Name">
-        <input type="text" name="" placeholder="Last Name">
-        <input type="text" name="" placeholder="ZIP Code">
-        <input type="number" name="" placeholder="Number of Employees to Service">
-        <input type="text" name="" placeholder="What is your Email Solution">
-        <input type="text" name="" placeholder="Industry">
-        <input type="text" name="" placeholder="What Do You Do For Tech Support">
-        <input type="text" name="" placeholder="Website">
-        <input type="text" name="" placeholder="Email">
-        <input type="text" name="" placeholder="Phone Number">
-          Which Plan Interests You? <br>
-       Business Basic <input name="group3" type="radio" > <br>
-      Business Premium <input name="group3" type="radio"><br>
-          <textarea rows="3" cols="50" name="comment" form="usrform" placeholder="Enter text here...">
-          </textarea>
-          <input type="submit" name="SUBMIT">
-      </form>
-    </div>
-    <div class="row">
-      <div class="col-md-2">
+	        	</div>
+		        <div class="col-md-3">
+		          <h2 style="color:blue; font-weight:bold;">Business <br> Basic Plan </h2>
+		          <span></span>
+		          <p>
+		            <i class='fas fa-check' style='font-size:20px; color:blue;'></i><b>Monthly Cost</b> <br>
+		            xx rupees per Employee <br>
+		            <i class='fas fa-check' style='font-size:20px; color:blue;'></i><b>Online Support</b> <br>
+		            6am to 6pm, Mon-Fri <br>
+		            <i class='fas fa-check' style='font-size:20px; color:blue;'></i><b>On-Site Support</b> <br>
+		            xx rupees <br>
+		            <i class='fas fa-check' style='font-size:20px; color:blue;'></i><b>Device Supported</b> <br>
+		            Unlimited <br>
+		          </p>
+		        </div>
+		        <div class="col-md-3">
+		          <h2 style="color:blue; font-weight:bold;">Business <br> Basic Premium Plan </h2>
+		          <span></span>
+		          <p>
+		            <i class='fas fa-check' style='font-size:20px; color:blue;'></i><b>  Monthly Cost</b> <br>
+		            xx rupees per Employee <br>
+		            <i class='fas fa-check' style='font-size:20px; color:blue;'></i><b>Online Support</b> <br>
+		            6am to 6pm, Mon-Fri <br>
+		            <i class='fas fa-check' style='font-size:20px; color:blue;'></i><b>On-Site Support</b> <br>
+		            xx rupees <br>
+		            <i class='fas fa-check' style='font-size:20px; color:blue;'></i><b>Device Supported</b> <br>
+		            Unlimited <br>
+		            <i class='fas fa-check' style='font-size:20px; color:blue;'></i><b>Remote Monitoring</b> <br>
+		            xxxxxxxxxxx <br>
+		            <i class='fas fa-check' style='font-size:20px; color:blue;'></i><b> Discounted Annual Contract (Min. 5 employees)</b> <br>
+		            15% Discounted <br>
+		          </p>
+		        </div>
+      		</div>
+      		<div class="row">
+        		<div class="col-md-2">
 
-      </div>
-      <div class="col-md-2">
-        <h2 style="color:blue; font-weight:bold;">Business <br> Basic Plan </h2>
-        <span></span>
-        <p>
-          <i class='fas fa-check' style='font-size:20px; color:blue;'></i><b>Monthly Cost</b> <br>
-          xx rupees per Employee <br>
-          <i class='fas fa-check' style='font-size:20px; color:blue;'></i><b>Online Support</b> <br>
-          6am to 6pm, Mon-Fri <br>
-          <i class='fas fa-check' style='font-size:20px; color:blue;'></i><b>On-Site Support</b> <br>
-          xx rupees <br>
-          <i class='fas fa-check' style='font-size:20px; color:blue;'></i><b>Device Supported</b> <br>
-          Unlimited <br>
-        </p>
-      </div>
-      <div class="col-md-3">
-        <h2 style="color:blue; font-weight:bold;">Business <br> Basic Premium Plan </h2>
-        <span></span>
-        <p>
-          <i class='fas fa-check' style='font-size:20px; color:blue;'></i><b>  Monthly Cost</b> <br>
-          xx rupees per Employee <br>
-          <i class='fas fa-check' style='font-size:20px; color:blue;'></i><b>Online Support</b> <br>
-          6am to 6pm, Mon-Fri <br>
-          <i class='fas fa-check' style='font-size:20px; color:blue;'></i><b>On-Site Support</b> <br>
-          xx rupees <br>
-          <i class='fas fa-check' style='font-size:20px; color:blue;'></i><b>Device Supported</b> <br>
-          Unlimited <br>
-          <i class='fas fa-check' style='font-size:20px; color:blue;'></i><b>Remote Monitoring</b> <br>
-          xxxxxxxxxxx <br>
-          <i class='fas fa-check' style='font-size:20px; color:blue;'></i><b> Discounted Annual Contract (Min. 5 employees)</b> <br>
-          15% Discounted <br>
-        </p>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-md-2">
+        		</div>
+		        <div class="col-md-5">
+		          *On-Site Next Business Day Support. Same Day Support is +$50/site visit.
+		        </div>
+      		</div>
+     		<div class="row">
+		        <div class="col-md-2">
 
-      </div>
-      <div class="col-md-5">
-        *On-Site Next Business Day Support. Same Day Support is +$50/site visit.
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-md-2">
+		        </div>
+		        <div class="col-md-5">
+		          <h2>Plans include the following services and more:</h2>
+		          <br><b>Employee IT Services</b> <br>
+		          <i class='fas fa-circle' style='font-size:10px' class="with-gap"></i>one <br>
+		          <i class='fas fa-circle' style='font-size:10px'></i>two <br>
+		          <i class='fas fa-circle' style='font-size:10px'></i> three <br> <br>
+		          <b>System Administration</b> <br>
+		          <i class='fas fa-circle' style='font-size:10px'></i>one <br>
+		          <i class='fas fa-circle' style='font-size:10px'></i>two <br>
+		          <i class='fas fa-circle' style='font-size:10px'></i>three <br> <br>
+		          <b>Proactive Support</b> <br>
+		          <i class='fas fa-circle' style='font-size:10px'></i>one <br>
+		          <i class='fas fa-circle' style='font-size:10px'></i>two <br>
+		          <i class='fas fa-circle' style='font-size:10px'></i>three <br> <br>
+		          <b>WiFi Services</b> <br>
+		          <i class='fas fa-circle' style='font-size:10px'></i>one <br>
+		          <i class='fas fa-circle' style='font-size:10px'></i>two <br>
+		          <i class='fas fa-circle' style='font-size:10px'></i>three <br> <br>
+		          <b>Other Services</b> <br>
+		          <i class='fas fa-circle' style='font-size:10px'></i>one <br>
+		          <i class='fas fa-circle' style='font-size:10px'></i>two <br>
+		          <i class='fas fa-circle' style='font-size:10px'></i>three <br> <br>
+		        </div>
+      		</div>
+      		<div class="row" style="padding:30px;">
+		        <div class="col-md-2">
 
-      </div>
-      <div class="col-md-5">
-        <h2>Plans include the following services and more:</h2>
-        <br><b>Employee IT Services</b> <br>
-        <i class='fas fa-circle' style='font-size:10px' class="with-gap"></i>one <br>
-        <i class='fas fa-circle' style='font-size:10px'></i>two <br>
-        <i class='fas fa-circle' style='font-size:10px'></i> three <br> <br>
-        <b>System Administration</b> <br>
-        <i class='fas fa-circle' style='font-size:10px'></i>one <br>
-        <i class='fas fa-circle' style='font-size:10px'></i>two <br>
-        <i class='fas fa-circle' style='font-size:10px'></i>three <br> <br>
-        <b>Proactive Support</b> <br>
-        <i class='fas fa-circle' style='font-size:10px'></i>one <br>
-        <i class='fas fa-circle' style='font-size:10px'></i>two <br>
-        <i class='fas fa-circle' style='font-size:10px'></i>three <br> <br>
-        <b>WiFi Services</b> <br>
-        <i class='fas fa-circle' style='font-size:10px'></i>one <br>
-        <i class='fas fa-circle' style='font-size:10px'></i>two <br>
-        <i class='fas fa-circle' style='font-size:10px'></i>three <br> <br>
-        <b>Other Services</b> <br>
-        <i class='fas fa-circle' style='font-size:10px'></i>one <br>
-        <i class='fas fa-circle' style='font-size:10px'></i>two <br>
-        <i class='fas fa-circle' style='font-size:10px'></i>three <br> <br>
-      </div>
-    </div>
-    <div class="row" style="padding:30px;">
-      <div class="col-md-2">
+		        </div>
+		        <div class="col-md-1">
+		          <i class='fas fa-circle' style='font-size:30px'></i>
+		        </div>
+		        <div class="col-md-5">
+		          We,re committed to your complete satisfaction <br>
+		          If you're not 100% happy, we'll do whatever it takes to make it right.
+		        </div>
+      		</div>
 
-      </div>
-      <div class="col-md-1">
-        <i class='fas fa-circle' style='font-size:30px'></i>
-      </div>
-      <div class="col-md-5">
-        We,re committed to your complete satisfaction <br>
-        If you're not 100% happy, we'll do whatever it takes to make it right.
-      </div>
-    </div>
+        	<div class="row" style="background:blue; height:130px;width:100%">
+	          	<div class="col-lg-12">
+	            	<h2 style="text-align:center;">"The best, chepest way to get good Tech support."</h2>
+	            	<br> 
+	            	<p style="text-align:center;font-size:20px; font-weight:bold"><i class='fas fa-circle' style='font-size:20px'></i> HelloTech</p> <br>
+	          	</div>
+        	</div>
+      		<div class="row" id="foot2" style="background:dimgrey;height:120px; width:100%; size:cover">
+		        <div class="col-lg-4">
 
-      <div class="row" style="background:blue; height:130px;width:100vw">
-        <div class="col-lg-12">
-          <h2 style="text-align:center;">"The best, chepest way to get good Tech support."</h2>
-          <br> <p style="text-align:center;font-size:20px; font-weight:bold"><i class='fas fa-circle' style='font-size:20px'></i> HelloTech</p> <br>
-        </div>
-      </div>
-    <div class="row" id="foot2" style="background:dimgrey;height:120px; width:100vw; size:cover">
-      <div class="col-lg-4">
-
-      </div
-      <div class="col-lg-8" style="padding:20px;">
-        <p style="padding:20px;">
-          FAQ  |  Terms  |  Privacy  | <br>
-          Onsite & Online Tech support, installation, setup and repair <br>
-          Copyright xxxxxxxxxxxxxx,All rights reserved <br>
-        </p>
-      </div>
-    </div>
+		        </div>
+		        <div class="col-lg-8" style="padding:20px;">
+		          <p style="padding:20px;">
+		            FAQ  |  Terms  |  Privacy  | <br>
+		            Onsite & Online Tech support, installation, setup and repair <br>
+		            Copyright xxxxxxxxxxxxxx,All rights reserved <br>
+		          </p>
+		        </div>
+      		</div>
+      	</div>
     </div>
 
     <!-- Optional JavaScript -->
